61d03f2d084b38d552998b78a0f5856ca4087c8f
vchuri-max <vrushal669@gmail.com>
2026-07-13 13:59:21 -0700

    Create 01_variables.py
    Add Python variables lesson

---
+++ 01_variables.py
@@ -0,0 +1,12 @@
+# Python Variables
+# A variable stores a value that we can use later.
+
+name = "test"
+age = 30
+height = 5.9
+is_learning_python = True
+
+print(name)
+print(age)
+print(height)
+print(is_learning_python)
